45979207db9682ad2ffecce82e10de4875833f6d
Clinton <iblamefish@gmail.com>
2015-07-05 20:18:38 +0100

    exercise 1 study drills

--- exercise-1/ex1.py
+++ exercise-1/ex1.py
@@ -5,3 +5,6 @@ print "This is fun"
 print "Yay! Printing."
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+
+print "Another line"
+# print "This will not print because it's commented out"
